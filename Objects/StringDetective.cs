cb1faa6fdd70acf9d69ce73bc165d3789ef1b16e
Labrynth <student@epicodus.com>
2016-02-19 11:33:40 -0800

    first draft of inter-string word detection

--- Objects/StringDetective.cs
+++ Objects/StringDetective.cs
@@ -8,18 +8,51 @@ namespace StringDetectiveNamespace
   {
     private string _candidateString;
     private string _targetString;
+    //private int array for occurances would be here I think
 
 
       public StringDetective (string stringToSearch, string target)
       {
         _candidateString=stringToSearch;
         _targetString=target;
+        //maybe put a data structure here to store locations of matches?
       }
 
       public int Search()
       {
         int numOfOccurances = 0;
+        int targetlength = 0;
+        int movesUntilOutOfBounds = 0;
+        char[] candidateToArray;
+        // char[] targetToArray; don't need this
+        string subSearchCandidate;
 
+        if (_candidateString == _targetString)  //testing if strings are 100% identical saves time, especially for big strings
+        {
+          numOfOccurances=-42;
+        }
+
+        else
+        {
+          candidateToArray = new char[_candidateString.Length];
+          candidateToArray = _candidateString.ToCharArray();
+          targetlength = _targetString.Length;
+          movesUntilOutOfBounds = _candidateString.Length - targetlength;
+
+          for (int i=0; movesUntilOutOfBounds > 0 ; i++)
+          {
+            Console.WriteLine("Search Coord:" + i + " " + movesUntilOutOfBounds);
+            subSearchCandidate= _candidateString.Substring( i, (i+movesUntilOutOfBounds-1) );
+            //  |xxx|xx   x|xxx|x   xx|xxx|
+            //  0-2        1-3        2-4
+            if (subSearchCandidate == _targetString)
+            {
+              numOfOccurances++;
+            }
+            movesUntilOutOfBounds--;
+          }
+
+        }
 
         return numOfOccurances;
       }
--- Tests/StringDetectiveTest.cs
+++ Tests/StringDetectiveTest.cs
@@ -11,7 +11,21 @@ namespace StringDetectiveNamespace
     {
       StringDetective newStringDetective = new StringDetective("cat", "cat");
       //  Console.WriteLine("Test output: " + newAnagram.GetInput());
-      Assert.Equal(1, newStringDetective.Search());
+      Assert.Equal(-42, newStringDetective.Search());
+    }
+    [Fact]
+    public void Search_partialdoesntclaimfullmatch_false()
+    {
+      StringDetective newStringDetective = new StringDetective("catcat", "cat");
+      //  Console.WriteLine("Test output: " + newAnagram.GetInput());
+      Assert.NotEqual(-42, newStringDetective.Search());
+    }
+    [Fact]
+    public void Search_returnoccurancesoffullword_true()
+    {
+      StringDetective newStringDetective = new StringDetective("catcatcat", "cat");
+      //  Console.WriteLine("Test output: " + newAnagram.GetInput());
+      Assert.Equal(3, newStringDetective.Search());
     }
 
 
